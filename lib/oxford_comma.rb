f26def457f42d07b400b723b6e2e8292ae096487
Kyle Imhoff <imhoff.kyle@gmail.com>
2020-02-17 22:47:05 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,7 +7,7 @@ def oxford_comma(array)
     array.join(" and ")
   
   elsif size > 2 
-    array[0..-2].join(', ') + " and " + array[-1] 
+    array[0..-2].join(', ') + ", and " + array[-1] 
   end
   
   
